3fffb9e28880509b068c386b28c905513dcb3fd3
Andy <111096875+andyjv1@users.noreply.github.com>
2023-06-17 16:05:44 -0400

    add path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const path = require('path')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
